528b545dd77a0f06867bd4fd2fb0992caf84d597
Henrique Ferraz <henriqueferraz@ofnet.com.br>
2025-03-14 15:35:34 -0300

    14/03

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -1,7 +1,32 @@
 import { Router } from 'express';
+import { createUser, createUsers } from '../services/user';
 
 export const mainRouter = Router();
 
 mainRouter.get('/ping', (req, res) => {
     res.json({ pong: true });
+});
+
+mainRouter.post('/user', async (req, res) => {
+
+    const user = await createUser({
+        name: req.body.name,
+        email: req.body.email,
+    });
+    if (user) {
+        res.status(201).json({ user });
+    } else {
+        res.status(400).json({ error: 'Erro ao criar usuário' });
+    }
+});
+
+
+mainRouter.post('/users', async (req, res) => {
+
+    const users = await createUsers(req.body.users);
+    if (users) {
+        res.status(201).json({ users });
+    } else {
+        res.status(400).json({ error: 'Erro ao criar usuários' });
+    }
 });
---
+++ src/services/user.ts
@@ -0,0 +1,19 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../libs/prisma";
+
+export const createUser = async (data: Prisma.UserCreateInput) => {
+
+    try {
+        return await prisma.user.create({ data });
+    } catch (error) {
+        return false;
+    }
+
+};
+
+export const createUsers = async (users: Prisma.UserCreateInput[]) => {
+
+    return await prisma.user.createMany({
+        data: users
+    });
+}
